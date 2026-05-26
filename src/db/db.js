cca231de4a558afaaeeb31d4487f0b1939282f28
harsh <harshsharmaff56@gmail.com>
2026-05-26 12:33:08 +0530

    second commit

--- src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-
-async function connectDB(){
-    
-    await mongoose.connect("mongodb+srv://hello:csUAVizhJl5MdLsi@freecluster.2ah5ark.mongodb.net/heloi")
-    console.log("connected to db")
-}
-
-module.exports =connectDB;
